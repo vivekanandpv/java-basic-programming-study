7f2a6fbaa40c5715e76466f8867f2d0186339b95
Vivekanand P V <vivek@anviksha.io>
2024-08-05 18:22:36 +0530

    done

--- src/io/anviksha/demo/Main.java
+++ src/io/anviksha/demo/Main.java
@@ -1,5 +1,6 @@
 package io.anviksha.demo;
 
+//  Please refer to the demonstration visuals in resources directory
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");

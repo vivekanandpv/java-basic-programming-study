e9096828e9c3b98cb6528b38c237a89bf640940a
Vivekanand P V <vivek@anviksha.io>
2024-08-05 22:12:32 +0530

    done

--- src/io/anviksha/demo/Main.java
+++ src/io/anviksha/demo/Main.java
@@ -3,8 +3,9 @@ package io.anviksha.demo;
 
 public class Main {
     public static void main(String[] args) {
-        //  check the constructor call dispatches through debugger
-        Point2D p = new Point2D();
+        //  Point2D p = new Point2D(2, 3);  //  constructor ambiguous call
         Point2D p2 = new Point2D(2.3, -8);
+
+        //  p.distanceTo(3, 4); //  Ambiguous method call
     }
 }
--- src/io/anviksha/demo/Point2D.java
+++ src/io/anviksha/demo/Point2D.java
@@ -4,28 +4,23 @@ public class Point2D {
     private double x;
     private double y;
 
-    //  if the multiple overloads are dispatchable, the most proximal type is chosen
-    //  this rule equally applies to method overloading
-    //  defining such overloads is not a good practice however
-    //  generally, method and constructor overloading should be done only for the most pressing cases
-    //  Please refer:
-    //  https://www.yegor256.com/2023/08/01/method-overloading.html
-    //  https://softwareengineering.stackexchange.com/questions/135313/when-is-method-overloading-appropriate
-    //  https://stackoverflow.com/a/38539515/3969961
-    public Point2D(double x, double y) {
+    //  mindless overloading can produce call resolution ambiguities at the call sites
+    //  such things do occur when your overloads start to differ by compatible and automatically convertible types
+    public Point2D(double x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public Point2D(int x, int y) {
-        this((double) x, (double) y);   //  now Point2D(double, double) is proximal
+    public Point2D(int x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public Point2D() {
-        //  0 is proximal to Point2D(int, int)
-        //  0.0 is proximal to Point2D(double, double)
-        this(0, 0);
+    public void distanceTo(double x, int y) {
+
     }
 
+    public void distanceTo(int x, double y) {
 
+    }
 }

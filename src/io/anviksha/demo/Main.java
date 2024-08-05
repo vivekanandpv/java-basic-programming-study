cc06055fe1eecea4390e7f1369b28491d50557b0
Vivekanand P V <vivek@anviksha.io>
2024-08-05 19:58:26 +0530

    done

--- src/io/anviksha/demo/Main.java
+++ src/io/anviksha/demo/Main.java
@@ -3,17 +3,26 @@ package io.anviksha.demo;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
+        //  https://en.wikipedia.org/wiki/Bitwise_operation
 
-        //  debug step by step to understand
+        int b = 15; //  binary: 1111
 
-        //  post-increment/decrement operators return and operate later
-        int j = i++;    //  j = 0, i = 1
-        int k = i--;    //  k = 1, i = 0
+        int c = b >> 1; //  right shift one bit: 1111 => 111
 
-        //  pre-increment/decrement operators operate and return later
-        int p = 0;
-        int q = ++p;    //  q = 1, p = 1
-        int r = --p;    //  r = 0, p = 0
+        int d = b << 1; //  left shift one bit: 1111 => 11110 = 30
+
+        int e = 10 & 7;  //  1010 (and) 0111 => 0010 = 2
+
+        int f = 10 | 5; //  1010 (or) 0101 => 1111 = 15
+
+        int g = 15 ^ 10;    //  1111 (xor) 1010 => 0101 = 5
+
+        //  recall that the int is a signed type, which follows 2's complement
+        //  Intuitively, you think (complement) 1010 ->  0101 = 5
+        //  but sign bit is also inverted!
+        //  hence the full 32 bit value is: 11111111111111111111111111110101 = -11
+        int h = ~10;    //  = -11
+
+        //  also available are >>=, <<=, &=, |=, ^=
     }
 }

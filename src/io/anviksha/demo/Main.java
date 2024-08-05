bbbed5eb657c5257fd0c1b76be6b4491724158b1
Vivekanand P V <vivek@anviksha.io>
2024-08-05 20:53:46 +0530

    done

--- src/io/anviksha/demo/Main.java
+++ src/io/anviksha/demo/Main.java
@@ -3,73 +3,25 @@ package io.anviksha.demo;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 4;
+        //  use for loop for definite iteration (when you know how many iterations beforehand)
 
-        //  if is a statement, else is a clause
-        //  only boolean expression is allowed
-        //  block, even for single statement is recommended
-        if (x > y) {
-            System.out.println("Hello!");
-        } else {
-            System.out.println("Good bye!");
+        //  i, j, k are usual loop counters
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello for loop!");
         }
 
-        //  else if is a nested if within else clause:
-
-        //  else {
-        //      if (cond) {
-        //
-        //      }
-        //  }
-
-        if (x > 5) {
-            System.out.println("Option 1");
-        } else if (x == 5) {
-            System.out.println("Option 2");
-        } else {
-            System.out.println("Option 3");
+        //  while and do-while loops for indefinite iterations (loops of conditional exit)
+        int j = 0;
+        while (j < 5) {
+            System.out.println("Hello while loop!");
+            ++j;
         }
 
-        //  switch for multi-option evaluation
-        //  JDK 7 feature
-        switch (x) {
-            //  the fall-through will happen if break (or return) is not used
-            //  beware of using return, as it returns from the current method
-            //  exhaustive case coverage is not mandatory
-            //  default case is optional
-
-            case 5:
-                System.out.println("x = 5");
-                break;
-            case 4:
-                System.out.println("x = 4");
-                break;
-            case 3:
-                System.out.println("x = 3");
-                break;
-            case 2:
-                System.out.println("x = 2");
-                break;
-            case 1:
-                System.out.println("x = 1");
-                break;
-            default:
-                System.out.println("Invalid x!");
-                break;
-        }
-
-        //  switch expression is a Java 17 feature
-        //  for pattern matching: https://docs.oracle.com/en/java/javase/17/language/pattern-matching-switch-expressions-and-statements.html
-
-        String output = switch (x) {
-            //  this is an expression, not a statement
-            //  cases should be exhaustive, or should have default
-            //  all cases should return the same type
-            case 5 -> "x = 5";
-            case 4 -> "x = 4";
-            case 3 -> "x = 3";
-            default -> "Invalid x";
-        };
+        //  do-while runs at least once, even if the condition is false
+        //  https://softwareengineering.stackexchange.com/questions/268145/are-there-real-life-usage-and-applications-for-do-while-loops
+        int k = 0;
+        do {
+            System.out.println("Hello do-while loop!");
+        } while(k > 0);
     }
 }

3c4cb3e04b77a9c69e4a6c62c5d7476e1d1ef68c
Vivekanand P V <vivek@anviksha.io>
2024-08-05 21:18:21 +0530

    done

--- src/io/anviksha/demo/BankAccount.java
+++ src/io/anviksha/demo/BankAccount.java
@@ -1,24 +1,21 @@
 package io.anviksha.demo;
 
 public class BankAccount {
-    //  a class has members
-    //  fields are data members (state)
-    //  methods are function members (behaviour)
-    //  relevant fields and methods make the proper encapsulation
-    //  exposing the relevant functionality to the clients (users) is abstraction
-
-    //  fields are kept private so that direct external access is controlled
-    //  if allowed, such direct access can disrupt the behaviour of the class
-    //  fields are also called class variables
-    //  when an object is created, these get the default values as zero-equivalents
-    //  (numbers get 0, booleans get false, reference types get null)
     private int number;
     private boolean active;
     private String holder;
     private double balance;
 
-    //  public methods are exposed to the client
-    //  set of these public methods is the API of the class
+    //  constructor is a special kind of method that is used to initialize class variables
+    //  constructor has the same name as the class, but no return type is mentioned
+    //  If there are no constructors, only then the compiler inserts a default constructor as:
+    //  public BankAccount() { }
+
+    //  explicit constructor is written for demonstration
+    public BankAccount() {
+        System.out.println("Inside the BankAccount constructor");
+    }
+
     public void deposit(double amount) {
         //  logic
     }
--- src/io/anviksha/demo/Main.java
+++ src/io/anviksha/demo/Main.java
@@ -3,22 +3,9 @@ package io.anviksha.demo;
 
 public class Main {
     public static void main(String[] args) {
-        //  First, take a look at the BankAccount.java file
+        BankAccount account = new BankAccount();    //  observe the call to the constructor on this line
 
-        //  BankAccount is a class, account is a variable that points to an object of that class
-
-        //  class is a blueprint and object is a realization of that
-        //  object is also called an instance of the class
-
-        //  new BankAccount() is often used to create an instance of the class
-        BankAccount account = new BankAccount();
-
-        //  API is now accessible on the object
-        //  calling methods on the object
         account.deposit(1220);
         account.withdraw(100);
-
-        //  private members are not accessible
-        //  double d = account.balance;
     }
 }

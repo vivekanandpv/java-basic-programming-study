bd05658bd3595b2bb4e500f9b9f2f213972706e6
Vivekanand P V <vivek@anviksha.io>
2024-08-05 23:00:35 +0530

    done

--- src/io/anviksha/demo/FourWheeler.java
+++
@@ -1,7 +0,0 @@
-package io.anviksha.demo;
-
-public class FourWheeler extends Vehicle {
-    public FourWheeler(String registrationNumber) {
-        super(registrationNumber);
-    }
-}
--- src/io/anviksha/demo/Main.java
+++ src/io/anviksha/demo/Main.java
@@ -2,12 +2,36 @@ package io.anviksha.demo;
 
 
 public class Main {
-    //  Before the constructor of a class is executed, it's superclass constructor is called
-    //  If the superclass has a parameterized constructor, then the subclass must pass the argument through super() call
-
     public static void main(String[] args) {
-        Car c = new Car("KA01AS9087");  //  trace the construction process
 
-        System.out.println(c.getRegistrationNumber());
     }
 }
+
+class Vehicle {
+    //  final fields should be initialized in place or in constructor
+    //  only one assignment is allowed, subsequent assignments are errors
+    //  setters cannot be declared, obviously
+    private final String registrationNumber;
+
+    public Vehicle(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    //  a final method cannot be overridden
+    public final void drive() {
+        System.out.println("Vehicle drives...");
+    }
+}
+
+//  a final class cannot be subclassed
+final class FourWheeler extends Vehicle {
+
+    public FourWheeler(String registrationNumber) {
+        super(registrationNumber);
+    }
+}
+
+//  error, cannot subclass final FourWheeler
+//class Car extends FourWheeler {
+//
+//}

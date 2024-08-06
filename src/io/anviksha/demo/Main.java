7013b29c4845243c095103b35e8493fffe9aa79c
Vivekanand P V <vivek@anviksha.io>
2024-08-06 06:35:26 +0530

    done

--- src/io/anviksha/demo/Main.java
+++ src/io/anviksha/demo/Main.java
@@ -1,66 +1,57 @@
 package io.anviksha.demo;
 
-class Sample {
-    //  by default, the execution happens in object/instance context
-    //  this is also called non-static context
-    //  by using static qualifier, members can be declared in static context,
-    //  which is the type context. static members cannot access instance context members,
-    //  but instance context members can access static members nevertheless
-    //  static members are accessed directly on the type (though in Java, static members
-    //  of a class can also be accessed on an object reference, such practice is not recommended)
+//  when components are separate, yet are combined to make a compositional entity
+//  the association relation is born.
 
-    private String city;
-    private static String country;  //  static field
+//  one class uses the other class/es as fields
 
-    static {
-        //  static initializer block
-        //  used to initialize static members
-        //  cannot take parameters
-        //  check the initialization order given below
-        country = "India";
+//  Engine and Transmission are component classes
+class Engine {
+    public void start() {
+        //  logic
     }
 
-    //  initialization order in Java
-    //  static fields
-    //  static initializer block
-    //  instance fields
-    //  constructor
+    public void stop() {
+        //  logic
+    }
+}
 
-    //  In Java, static final combination is used to create a constant
-    //  https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/lang/Math.html#PI
-    //  constants are written in all caps with _ to separate words
-    public static final double PI = 3.1415926535;
+class Transmission {
 
-    public void foo() {
-        //  instance member
-        //  static members are accessible from this context
-        bar();
+}
+
+//  Car is composed on Engine and Transmission
+class Car {
+    //  this association creates dependencies
+
+    //  if the Engine or Transmission can be used independently of Car
+    //  then the relation is called aggregation
+    //  if not, the relation is called composition
+    //  Aggregation is relatively looser than composition
+    private Engine engine;  //  Car has an Engine
+    private Transmission transmission;  //  Car has a transmission
+
+    //  dependencies are inverted, preferably in constructor
+    //  this is to relinquish the control over dependency's life cycle
+    //  also called Inversion of Control (IoC). nowadays known as Dependency Inversion (DI)
+    public Car(Engine engine, Transmission transmission) {
+        this.engine = engine;
+        this.transmission = transmission;
     }
 
-    //  static methods are not inherited
-    //  hence they cannot be overridden (static methods are final by design)
+    private void start() {
+        //  this is called delegation
+        engine.start();
+    }
 
-    //  constructors cannot be made static
-    //  Java doesn't have the concept of static class as C#
-    //  However, this could be achieved through Java 8 interfaces
-
-    //  static methods can also be overloaded like normal methods
-    //  same rules of overloading apply
-    public static void bar() {
-        System.out.println("static bar method");
-        System.out.println(country);    //  ok; static members are accessible
-        //  System.out.println(city);   //  error; cannot access non-static members
+    private void stop() {
+        engine.stop();
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        //  static members are accessed on type
-        Sample.bar();
-        System.out.println(Sample.PI);
 
-        Sample s = new Sample();
-        s.bar();    //  allowed; but not recommended
     }
 }
 

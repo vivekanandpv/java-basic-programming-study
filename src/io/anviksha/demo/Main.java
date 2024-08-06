37137e8467feb8dec1aa879510bf3d2685b31e66
Vivekanand P V <vivek@anviksha.io>
2024-08-06 23:30:38 +0530

    done

--- src/io/anviksha/demo/Main.java
+++ src/io/anviksha/demo/Main.java
@@ -1,51 +1,68 @@
 package io.anviksha.demo;
 
+class Sample {
+    private int year = 2024;
+
+    //  nested classes are used in some advanced situations such as:
+    //  1. logically grouping the classes that are only used in one place
+    //  2. when a class needs to know the private implementation of the other
+    //  3. nesting small classes within top-level classes places the code
+    //  closer to where it is used
+
+    //  serialization of inner classes is strongly discouraged
+    //  as there may be difference in the bytecode emitted by different compiler implementations
+
+    //  this nested class is a member of the Sample class
+    //  this is called a static nested class
+    //  static nested classes cannot refer to instance members defined in the outer class
+    //  In effect, the static nested class is behaviourally an independent class
+    //  that has been nested in another top-level class for packaging convenience
+    //  even though the class is static, the members can be instance members
+    //  static refers to the availability of the class
+    public static class Demo {
+        private final String message;
+
+        public Demo(String message) {
+            this.message = message;
+        }
+
+        public int foo() {
+            return 123;
+        }
+
+        public void bar() {
+            System.out.println(message);
+        }
+    }
+
+    //  this too is a member of the Sample class called inner class
+    //  an inner class is always associated with an instance of the outer class
+    //  to create an instance of the Playground,
+    //  first the instance of Sample needs to be created
+    public class Playground {
+        //  fields of the inner class do shadow the fields of the outer class
+        private final int score;
+
+        public Playground(int score) {
+            this.score = score;
+        }
+
+        public void foo() {
+            System.out.println(year);
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //  Strings are immutable. You cannot change a string object in place.
-        //  All these methods return new strings, which can be used to "replace" the old string
-        //  The old string will then get garbage collected
+        //  static nested class instantiation
+        Sample.Demo d = new Sample.Demo("Hello, world!");
+        int v = d.foo();
+        d.bar();
 
-        //  case conversion
-        String s1 = "Hello".toUpperCase();
-        String s2 = "Hello".toLowerCase();
-
-        //  indexing
-        char c = "Hello".charAt(2); //  get character at index
-        int i = "Hello".indexOf('l');   //  first index from start
-        int j = "Hello".lastIndexOf('l');   //  last index from start
-        int k = "Hello".indexOf('x');   //  -1 if not found
-
-        //  substring
-        String ss1 = "Hello".substring(2);  //  [2, end]
-        String ss2 = "Hello".substring(1, 3);   //  [1, 3)
-
-        //  splitting
-        String[] parts = "green;blue;red".split(";");   //  split by regex or separator
-
-        //  contains
-        boolean contains = "Hello".contains("ll");
-
-        //  regex match
-        boolean matches = "ABCD1234".matches("^[A-Z]{4}[0-9]{4}$");
-
-        //  stripping
-        String s3 = "     Hello World!   ".strip();
-        String s4 = "     Hello World!   ".stripLeading();
-        String s5 = "     Hello World!   ".stripTrailing();
-
-        //  though += is overloaded for strings, it is not efficient given strings are immutable
-        //  hence, StringBuilder and StringBuffer are useful
-        //  StringBuffer has the same API as StringBuilder
-        //  StringBuffer has synchronized methods, suited for multithreaded programming
-        //  StringBuilder has non synchronized methods
-
-        StringBuilder sbuilder = new StringBuilder();
-        sbuilder.append("Hi");
-        sbuilder.append(" ");
-        sbuilder.append("there!");
-        sbuilder.append(" ");
-
-        String finalString = sbuilder.toString();
+        //  inner class instantiation
+        Sample s = new Sample();
+        Sample.Playground p = s.new Playground(122);
+        p.foo();
     }
 }

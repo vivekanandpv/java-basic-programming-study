16b758f1eff15be7e200f59076c7913fe8d3764f
Vivekanand P V <vivek@anviksha.io>
2024-08-06 13:04:52 +0530

    done

--- src/io/anviksha/demo/Main.java
+++ src/io/anviksha/demo/Main.java
@@ -1,57 +1,45 @@
 package io.anviksha.demo;
 
-//  abstract classes are used as type, but they cannot be used as values
-//  these are used to provide infrastructure to the subclasses
-//  like regular classes, they can have fields and constructors
-//  abstract class must be subclassed, hence abstract final isn't allowed
-//  abstract classes may or may not have any abstract members
-//  abstract methods can only be declared in an abstract class
-//  only methods can be abstract
-//  such methods cannot have implementation (body)
-//  when a class extends an abstract class, it must implement all the abstract methods
-//  of the superclass, unless the subclass is also an abstract class
-//  a class cannot extend multiple abstract classes
-//  abstract class can also implement interfaces, in which they may or may not provide the implementation
-//  refer to Jva Collection API for the use of abstract classes and interfaces
-//  In JDK, the Abstract prefix is used for abstract classes
-
-abstract class Vehicle {
-    //  fields cannot be abstract
-    private final String registrationNumber;
-
-    //  constructors cannot be abstract
-    public Vehicle(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-    //  abstract methods cannot have any implementation (body)
-    public abstract void drive();
-
-    //  abstract classes can have non-abstract methods
-    public void honk() { }
-
-    //  abstract classes can have final methods
-    public final void start() { }
-}
-
-class Car extends Vehicle {
-
-    public Car(String registrationNumber) {
-        super(registrationNumber);
-    }
-
-    @Override
-    public void drive() {
-
-    }
-}
-
-
 public class Main {
     public static void main(String[] args) {
-        //  Vehicle can only be used as type
-        //  value has to be any concrete subclass of Vehicle, such as Car
-        Vehicle v = new Car("KA01AS9876");  //  polymorphism
-        v.drive();
+        //  arrays are fixed size, ordered, indexable, contiguously allocated collections of similar elements
+        //  array type can be primitive or user-defined
+        //  refer to: https://stackoverflow.com/a/17559021/3969961
+
+        //  idiomatic approach
+        int[] ar1;
+
+        //  C-style declaration; not recommended
+        int ar2[];
+
+        ar1 = new int[5];   //  array of 5 elements; all initialized to 0 (or equivalent)
+
+        ar2 = new int[] {4, 3, 5, 0};   //  regular initializer with elements; size is not allowed in this case
+
+        //  direct initializer approach; doesn't work to produce the standalone value of arrays,
+        //  such as passing the array arguments in place in a method call
+        int[] ar3 = {4, 3, 2, 1};
+
+        //  arrays are indexable; index starts with 0
+        //  array element access outside the bound throws a runtime exception
+        System.out.println(ar1[0]);
+
+        //  arrays elements can be mutated by indexes
+        //  array element access outside the bound throws a runtime exception
+        ar1[0] = 23;
+
+
+
+        //  arrays have (readonly) length property
+        System.out.println(ar1.length);
+
+        //  arrays are objects behind the scene
+        //  https://docs.oracle.com/javase/specs/jls/se8/html/jls-10.html
+        ar2 = null;
+
+        //  arrays can be used with for-each loop
+        for (int i : ar3) {
+            System.out.println(i);
+        }
     }
 }
